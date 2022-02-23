d0d121b50ff05d0ad7c0d81e511d15b798a037ee
Avatech-hk <100199672+Avatech-hk@users.noreply.github.com>
2022-02-23 12:13:06 +0800

    Version update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,11 +3,11 @@
 # -- Project information
 
 project = 'ARGEO Portable'
-copyright = '2021, Avatech'
+copyright = '2022, Avatech'
 author = 'Avatechhk'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
